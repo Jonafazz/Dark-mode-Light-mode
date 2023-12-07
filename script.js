80d2d3986111682c177e68bd191695c49b0c9fd3
Jona Fazliu <jonafazliu07@gmail.com>
2023-12-07 17:08:06 +0100

    detyra e pare me dark/light mode

---
+++ script.js
@@ -0,0 +1,9 @@
+function light(){
+    var light = document.body
+    light.classList.toggle('dark-mode')
+    var dark = document.querySelector('.alo')
+    dark.classList.toggle('rreth-dark')
+  
+} 
+//classList ka access ne style
+// toggle - event  
